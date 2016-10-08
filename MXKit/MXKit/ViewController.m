6fb71ac59468335633f78a41cdfe168dc6e42766
admin <you@example.com>
2016-10-08 22:05:18 +0800

    再次初始化仓库

--- MXKit/MXKit/ViewController.m
+++ MXKit/MXKit/ViewController.m
@@ -19,11 +19,14 @@
    
     
     
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    
 }
 
 @end
