8f02b02d44aea72fd98233dc786571d8f195fe52
fusen <fusen@163.com>
2017-09-01 16:16:51 +0800

    创建branch1类

---
+++ MXKit/FSBranch1.h
@@ -0,0 +1,13 @@
+//
+//  FSBranch1.h
+//  MXKit
+//
+//  Created by 付森 on 2017/9/1.
+//  Copyright © 2017年 MaXing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSBranch1 : NSObject
+
+@end
---
+++ MXKit/FSBranch1.m
@@ -0,0 +1,13 @@
+//
+//  FSBranch1.m
+//  MXKit
+//
+//  Created by 付森 on 2017/9/1.
+//  Copyright © 2017年 MaXing. All rights reserved.
+//
+
+#import "FSBranch1.h"
+
+@implementation FSBranch1
+
+@end
